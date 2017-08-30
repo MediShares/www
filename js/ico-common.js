2e88bb10bd04c3e84e8cb690d7b840e4b978d65c
tangying <1020278488@qq.com>
2017-08-30 23:24:32 +0800

    发展路线图
    发展路线图自定义滚动条

---
+++ js/ico-common.js
@@ -0,0 +1,7 @@
+var myScroll;
+
+function loaded () {
+	myScroll = new IScroll('#wrapper', { scrollX: true,scrollY: false, scrollbars: 'custom',interactiveScrollbars:true });
+}
+
+document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
